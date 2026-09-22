522318c8b9dd5c3a16f8a8e55e8752ad7a7a3bfc
leekangjae1 <10qkfemfndhk@gmail.com>
2026-09-23 00:10:47 +0900

    Update agent2_kg_analysis.py

--- agents/agent2_kg_analysis.py
+++ agents/agent2_kg_analysis.py
@@ -71,7 +71,7 @@ OUTPUT_FILE = BASE_DIR / "agent2_results.json"
 
 NEO4J_URI = os.getenv(
     "NEO4J_URI",
-    "neo4j+s://a08a329cdfca051f6e1c28f4087b995c.bolt.neo4jsandbox.com:443"
+    "bolt+s://a08a329cdfca051f6e1c28f4087b995c.neo4jsandbox.com:7687"
 )
 
 NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
